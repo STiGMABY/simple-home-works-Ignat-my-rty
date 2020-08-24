b4a19a22d8287b0a34d67ae0dc4626257b4b160b
Andrei <stigmaby@gmail.com>
2020-08-24 12:55:04 +0300

    My first commit

---
+++ src/p2-homeworks/h5/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {PATH} from "./Routes";
+
+function Header() {
+    return (
+        <div>
+            // add NavLinks
+            <NavLink to={PATH.PRE_JUNIOR}>PRE_JUNIOR </NavLink>
+            <NavLink to={PATH.JUNIOR}>JUNIOR </NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>JUNIOR_PLUS </NavLink>
+        </div>
+    );
+}
+
+export default Header;
