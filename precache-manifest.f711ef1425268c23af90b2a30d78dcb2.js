70de306cbb519f88d2d3757bac3de4f26a65cf97
Andrei <stigmaby@gmail.com>
2020-08-24 13:07:55 +0300

    Updates

---
+++ precache-manifest.f711ef1425268c23af90b2a30d78dcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805f5f6b64deffddcc347041a3728773",
+    "url": "/simple-home-works-Ignat-my-rty/index.html"
+  },
+  {
+    "revision": "5e282f12425bbe5bf9eb",
+    "url": "/simple-home-works-Ignat-my-rty/static/css/main.58649850.chunk.css"
+  },
+  {
+    "revision": "6a6e4120db557f5adbd2",
+    "url": "/simple-home-works-Ignat-my-rty/static/js/2.763d789d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-home-works-Ignat-my-rty/static/js/2.763d789d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e282f12425bbe5bf9eb",
+    "url": "/simple-home-works-Ignat-my-rty/static/js/main.80e090b6.chunk.js"
+  },
+  {
+    "revision": "575771eedd50f9504e82",
+    "url": "/simple-home-works-Ignat-my-rty/static/js/runtime-main.31d6ecd8.js"
+  }
+]);
